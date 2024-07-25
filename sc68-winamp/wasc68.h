36f5ce7250c41dc0f7eb0e657e457cd8f199357f
Darren Owen <winamp.plugins@gmail.com>
2024-07-25 12:34:11 +0100

    Updated the library

--- sc68-winamp/wasc68.h
+++ sc68-winamp/wasc68.h
@@ -59,7 +59,7 @@
  * the original value can be found in the configure.ac file.
  */
 #ifndef PACKAGE_VERSION
-# define PACKAGE_VERSION "1.0.9"
+# define PACKAGE_VERSION "1.0.10"
 #endif
 
 #include "sc68/sc68.h"
